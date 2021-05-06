1e4fca491cd2ba8e6fe99c7ba0d872884419e938
ZachLudwick <zachludwick@icloud.com>
2021-05-06 21:17:43 +0100

    Auth update, UI Refinements

--- js/Auth.js
+++ js/Auth.js
@@ -1,17 +1,10 @@
-var user = firebase.auth().currentUser;
-var uid = user.uid;
-
+var docId = document.getElementById;
+var auth = firebase.auth();
 // hello avacardo - Ella-May spakman 2021
-
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         // User is signed in.
 
-        document.getElementById('nav-user-div').style.display = "block";
-        document.getElementById('nav-login-div').style.display = "none";
-
-        document.getElementById("user_div").style.display = "block";
-        document.getElementById("login_div").style.display = "none";
 
         var user = firebase.auth().currentUser;
 
@@ -19,115 +12,45 @@ firebase.auth().onAuthStateChanged(function(user) {
 
             var email_id = user.email;
             var user_uid = user.uid;
-            document.getElementById("user_para").innerHTML = "Welcome User : " + email_id + "\n User Uid : " + user_uid;
 
-            document.getElementById('auth-login-button').innerHTML = "Open"
         }
 
     } else {
         // User isn't singed in.
-        document.getElementById('register-user-div').style.display = "none";
 
-        document.getElementById('nav-user-div').style.display = "none";
-        document.getElementById('nav-login-div').style.display = "block";
-
-        document.getElementById("user_div").style.display = "none";
-        document.getElementById("login_div").style.display = "block";
-
-
+        // Code
     }
 });
 
-function loginButtonClick() {
-    document.getElementById('login_div').style.display = "block";
-    document.getElementById('register-user-div').style.display = "none";
+function resetPasswordClick() {
+    window.alert('Soon!')
 }
 
-function registerButtonClick() {
-    document.getElementById('register-user-div').style.display = "block";
-    document.getElementById('login_div').style.display = "none";
-}
+function registerClicked() { }
 
 function register() {
-    // Auth values
-    var userEmail = document.getElementById('register_email_field').value;
-    var userPass = document.getElementById('register_password_field').value;
+    window.alert('Fucntion not ready yet.')
+}
 
-    // Database extra info.
-    var userFullname
-    var userName
-    var userJob
-    var userBio
+function loginClicked() { }
 
-    // User vars
+function login() {
+    var emailTextField = docId('login_email_textField').value;
+    var passwordTextField = docId('login_pass_textField').value;
 
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
+    auth.signInWithEmailAndPassword(emailTextField, passwordTextField)
         .then((userCredential) => {
             // Signed in
             var user = userCredential.user;
-            location.reload()
+            var currentUser = user.email;
+            window.alert(currentUser.email)
         })
         .catch((error) => {
             var errorCode = error.code;
             var errorMessage = error.message;
-
-            window.alert("Error : " + errorMessage + errorCode)
         })
 }
 
-function openUser() {
-    var user = firebase.auth().currentUser;
-    var uid = user.uid;
-
-
-}
-
-function getUserInfo() {
-    var docRef = db.collection("users").doc(uid);
-
-    docRef.get().then((doc) => {
-        if (doc.exists) {
-            console.log('Docuemnt data: ' + doc.data());
-        } else {
-            console.log('No such document!');
-        }
-    }).catch((error) => {
-        console.log("Error fetching user document from API", error)
-    })
-
-}
-
-function getPosts() {
-    var postId
-    var docRef = db.collection("posts").doc(postId);
-
-    docRef.get().then((doc) => {
-        if (doc.exists) {
-            console.log("Total Posts (From post Id) :" + doc.data())
-        } else {
-            console.log("No posts found")
-        }
-    }).catch((error) => {
-        console.log("Error Fetching User Posts (via. PostId) Dev Error: API Call Error in function getPosts() ...", errors)
-    })
-}
-
-function login(){
-
-    var userEmail = document.getElementById("email_field").value;
-    var userPass = document.getElementById("password_field").value;
-
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        window.alert("Error : " + errorMessage + errorCode);
-
-        // ...
-    });
-
-}
 
 function logout(){
     firebase.auth().signOut();
--- main.js
+++
@@ -1,1 +0,0 @@
-var userAgent = navigator.userAgent;
