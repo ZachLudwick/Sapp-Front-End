4f28ef3a2882790edfdd1063cfb810d32363cd19
ZachLudwick <zachludwick@icloud.com>
2021-05-10 22:15:23 +0100

    Auth Error handling with pass reset.

--- js/Auth.js
+++ js/Auth.js
@@ -1,3 +1,6 @@
+// Error codes.
+var errInvalidEmail = "auth/invalid-email";
+var errUserNotFound = "auth/user-not-found";
 
 // hello avacardo - Ella-May spakman 2021
 firebase.auth().onAuthStateChanged(function(user) {
@@ -31,16 +34,52 @@ function resetPasswordClick() {
     document.getElementById('reset-pw-div').style.display = "block";
 }
 
-function resetPasswordSubmit() {}
+function resetPasswordSubmit() {
+    var auth = firebase.auth();
+    var emailAddress = document.getElementById('forgot_email_textField').value;
+
+    auth.sendPasswordResetEmail(emailAddress).then(function () {
+        // Email Sent.
+        console.log('Succsess. Email sent to ' + emailAddress)
+        document.getElementById('reset-pw-error-span').style.display = "none";
+        document.getElementById('reset-pw-success-span').style.display = "block";
+    })
+        .catch(function (error) {
+        // Handle errors.
+        console.log('Yeah Something is wrong here.')
+        var errorMessage = error.message;
+        var errorCode = error.code;
+        console.log('ERROR AT: ' + errorCode);
+        document.getElementById('reset-pw-error-span').style.display = "none";
+
+        if (errorCode == errInvalidEmail) {
+            document.getElementById('reset-pw-error-span').style.display = "block";
+            document.getElementById('reset-pw-error-span').innerHTML = "Incorrect email format.";
+        }
+
+        else if (errorCode == errUserNotFound) {
+            document.getElementById('reset-pw-error-span').style.display = "block";
+            document.getElementById('reset-pw-error-span').innerHTML = "User not found.";
+        }
+
+    });
+}
 
 function registerClicked() {
     document.getElementById('log-in-sec').style.display = "none";
     document.getElementById('reg-user-sec').style.display = "block";
-    //window.alert('Registration is not quite ready just yet.')
+}
+
+function nextPageRegistration() {
+    if (document.getElementById('register_password_field').value < 6) {
+        window.alert("Yeah nah this ain't happenin")
+    } else {
+        window.alert("BRO UR GOOD!")
+    }
 }
 
 function register() {
-    window.alert('Fucntion not ready yet.')
+
 }
 
 function loginClicked() {
